b5e7b21660433114932d224fae0af1fc014ab65a
Folahanmi Kolawole <fola@aeeiee.com>
2021-10-15 14:30:10 +0100

    create project

---
+++ GHFollowers/GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by FOLAHANMI KOLAWOLE on 15/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
