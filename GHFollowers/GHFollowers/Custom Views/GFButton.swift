6a82060dc939ef4a25d1cbf70d5395480baf5abf
Folahanmi Kolawole <fola@aeeiee.com>
2021-10-18 13:59:39 +0100

    create custom button, custom textField and string constants

--- GHFollowers/GHFollowers/Custom Views/GFButton.swift
+++ GHFollowers/GHFollowers/Custom Views/GFButton.swift
@@ -29,8 +29,10 @@ class GFButton: UIButton {
   
   private func configure() {
     layer.cornerRadius = 10
+    
     titleLabel?.textColor = .white
     titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+    
     translatesAutoresizingMaskIntoConstraints = false
   }
 }
---
+++ GHFollowers/GHFollowers/String Constants/StringConstants.swift
@@ -0,0 +1,14 @@
+//
+//  StringConstants.swift
+//  GHFollowers
+//
+//  Created by FOLAHANMI KOLAWOLE on 18/10/2021.
+//
+
+import Foundation
+
+enum StringConstants {
+  enum GFTextField {
+    static let placeholderText = "Enter a username"
+  }
+}
