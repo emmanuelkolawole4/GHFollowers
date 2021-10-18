39cec8eecc4814f89392d1825e9d84ec94c01be2
Folahanmi Kolawole <fola@aeeiee.com>
2021-10-18 13:18:41 +0100

    create inital tabbar controller with search and favorites list view controllers embeded in

---
+++ GHFollowers/GHFollowers/Controllers/FavoritesListVC.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesListVC.swift
+//  GHFollowers
+//
+//  Created by FOLAHANMI KOLAWOLE on 17/10/2021.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemBlue
+  }
+}
--- GHFollowers/GHFollowers/Controllers/SearchVC.swift
+++ GHFollowers/GHFollowers/Controllers/SearchVC.swift
@@ -1,20 +1,16 @@
 //
-//  ViewController.swift
+//  SearchVC.swift
 //  GHFollowers
 //
-//  Created by FOLAHANMI KOLAWOLE on 15/10/2021.
+//  Created by FOLAHANMI KOLAWOLE on 17/10/2021.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class SearchVC: UIViewController {
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    
     view.backgroundColor = .systemPink
   }
-
-
 }
-
