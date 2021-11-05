64882419f9dfd4997ff152dcc382b373dc2a86bd
Folahanmi Kolawole <fola@aeeiee.com>
2021-11-05 12:27:37 +0100

    initialize collection view

--- GHFollowers/GHFollowers/Controllers/FollowerListVC.swift
+++ GHFollowers/GHFollowers/Controllers/FollowerListVC.swift
@@ -11,12 +11,35 @@ class FollowerListVC: UIViewController {
   
   // MARK: - PROPERTIES
   var username: String!
+  var collectionView: UICollectionView!
   
   // MARK: - VIEW LIFECYCLE METHODS
   override func viewDidLoad() {
     super.viewDidLoad()
+    configureViewController()
+    configureCollectionView()
+    getFollowers()
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    navigationController?.setNavigationBarHidden(false, animated: true)
+  }
+  
+  // MARK: - CUSTOM FUNCTIONS
+  func configureCollectionView() {
+    collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+    view.addSubview(collectionView)
+    collectionView.backgroundColor = .systemPink
+    collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseIdentifier)
+  }
+  
+  func configureViewController() {
     view.backgroundColor = .systemBackground
     navigationController?.navigationBar.prefersLargeTitles = true
+  }
+  
+  func getFollowers() {
     NetworkManager.shared.getFollowers(for: username, page: 1, perPageFollowers: 100) { result in
       switch result {
       case .failure(let error):
@@ -25,10 +48,5 @@ class FollowerListVC: UIViewController {
         dump("Followers.count: \(followers.count)")
       }
     }
-  }
-  
-  override func viewWillAppear(_ animated: Bool) {
-    super.viewWillAppear(animated)
-    navigationController?.setNavigationBarHidden(false, animated: true)
   }
 }
