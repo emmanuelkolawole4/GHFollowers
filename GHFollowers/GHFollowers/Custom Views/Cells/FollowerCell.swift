03288b7f90809d6f25d359d5c331eb67b4aba7d6
Folahanmi Kolawole <fola@aeeiee.com>
2021-11-10 14:42:50 +0100

    implement downloading avatar images from follower avatar url string

--- GHFollowers/GHFollowers/Custom Views/Cells/FollowerCell.swift
+++ GHFollowers/GHFollowers/Custom Views/Cells/FollowerCell.swift
@@ -28,6 +28,7 @@ class FollowerCell: UICollectionViewCell, ReuseIdentifying {
   // MARK: - CUSTOM FUNCTIONS
   func set(follower: Follower) {
     usernameLabel.text = follower.login
+    avatarImageView.downloadImage(from: follower.avatarUrl)
   }
   
   private func configure() {
--- GHFollowers/GHFollowers/Custom Views/GFAvatarImageView.swift
+++ GHFollowers/GHFollowers/Custom Views/GFAvatarImageView.swift
@@ -29,4 +29,25 @@ class GFAvatarImageView: UIImageView {
     
     translatesAutoresizingMaskIntoConstraints = false
   }
+  
+  func downloadImage(from urlString: String) {
+    guard let url = URL(string: urlString) else { return }
+    
+    let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+      guard let self = self else { return }
+      if let _ = error { return }
+      guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { return }
+      guard let data = data else { return }
+      
+      guard let image = UIImage(data: data) else {
+        dump("error creating image from data: \(error)")
+        return
+      }
+      
+      DispatchQueue.main.async {
+        self.image = image
+      }
+    }
+    task.resume()
+  }
 }
